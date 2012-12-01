e5efc9578320668e0dded3a540356ebae44bd1c2
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-01 01:30:40 +0000

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabblinGs::Application.config.secret_token = '778b340f54389ab7f9823717cbec03e211cff84005e1f3cc23713e9e16cbbce6ff9eede712369c996f1cf15351a461f27a63b00c36af2c1126d54a57d2515c74'
