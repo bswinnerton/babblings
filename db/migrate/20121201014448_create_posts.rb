45d7c75dc8c8ee72a4ed91af3e3bc6d94366f306
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-01 02:57:39 +0000

    initial creation of database and posts model

---
+++ db/migrate/20121201014448_create_posts.rb
@@ -0,0 +1,18 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+			t.integer :author
+			t.boolean :is_deleted, :default => 0
+			t.boolean :is_hidden, :default => 0
+      t.timestamps
+			t.string :type
+			t.text :title
+			t.text :content
+			t.text :original_path
+			t.integer :width
+			t.integer :height
+			t.integer :width_thumbnail
+			t.integer :height_thumbnail
+    end
+  end
+end
