f034f6c96bd6b90b80b321cae229c04b684c6444
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-02 19:53:21 +0000

    query improvements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.select('id, post_type, title, content, width_thumbnail, height_thumbnail').where('is_deleted' => false, 'is_hidden' => false).order('created_at DESC')
   end
 end
