267cd6a6ea3ac5eba5bcdd2e7e84fdd478898083
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-03 03:24:09 +0000

    start to post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.where('is_deleted' => false, 'is_hidden' => false).limit(15).order('created_at DESC')
   end
+
   def show
     @posts = Post.find(params[:id], :conditions => 'is_deleted = false')
   end
+
   def page
     @posts = Post.where('is_deleted' => false, 'is_hidden' => false).limit(15).offset(params[:page].to_i * 15).order('created_at DESC')
     render :layout => false
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    raise params.inspect
   end
 end
