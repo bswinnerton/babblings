82e4924b5fbfaea85767dd4a6cb65f0799482a61
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-03 03:23:41 +0000

    restructure post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  # attr_accessible :title, :body
-  attr_accessible :id, :post_type, :title, :content, :width, :height, :width_thumbnail, :height_thumbnail
+  # attr_accessible :id, :post_type, :title, :content, :width, :height, :width_thumbnail, :height_thumbnail
+  validates :content, :presence => true
 end
