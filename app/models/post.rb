f7ab666cac3930f39f9ab8f60fdddf7e698f40cd
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-06 15:05:30 +0000

    method to determine type

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,14 @@ class Post < ActiveRecord::Base
   def set_type
     if self.content =~ /(\.jpg|\.png|\.bmp|\.gif)$/
       self.post_type = 'image'
+    elsif self.content =~ /youtube.com/
+      self.post_type = 'youtube'
+    elsif self.content =~ /vimeo.com/
+      self.post_type = 'vimeo'
+    elsif self.content =~ /spotify:track/
+      self.post_type = 'spotify'
+    else
+      self.post_type = 'text'
     end
   end
 end
