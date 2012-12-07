00697dc7892756af400ef947e6da9a6dcd774790
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-07 17:42:24 +0000

    fix for broken functional test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.where('is_deleted' => false, 'is_hidden' => false).limit(15).order('created_at DESC')
+    @posts = Post.where(:is_deleted => 0, :is_hidden => 0).limit(15).order('created_at DESC')
   end
 
   def show
-    @posts = Post.find(params[:id], :conditions => 'is_deleted = false')
+    @posts = Post.find(params[:id], :conditions => 'is_deleted = 0')
   end
 
   def page
-    @posts = Post.where('is_deleted' => false, 'is_hidden' => false).limit(15).offset(params[:page].to_i * 15).order('created_at DESC')
-    render :layout => false
+    @posts = Post.where(:is_deleted => 0, :is_hidden => 0).limit(15).offset(params[:page].to_i * 15).order('created_at DESC')
+    render :layout => 0
   end
   
   def new
--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should get index" do
+		#puts Post.all
+
+		get :index
+		#puts @response.body
+		assert_response :success
+		assert_select '.contentBox', minimum: 5
+	end
 end
