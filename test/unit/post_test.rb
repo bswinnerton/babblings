2150ec9a0b59273b8ba70f19a004463d0a3de01e
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-07 18:26:07 +0000

    formatting update for unit test

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  fixtures :posts
+
 	test "post attributes must not be empty" do
 		post = Post.new
 		assert post.invalid?
 		assert post.errors[:content].any?
 	end
+
 end
