09b992815d5201fb910859df6c2e5e39cf22e4ef
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-07 02:16:50 +0000

    beginning unit test

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :posts
+	test "post attributes must not be empty" do
+		post = Post.new
+		assert post.invalid?
+		assert post.errors[:content].any?
+	end
 end
