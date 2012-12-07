225cdff81bbb65d78f561aff7817b26819a3b7d2
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-07 18:19:41 +0000

    functional test improvements

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,12 +1,28 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+
 	test "should get index" do
 		#puts Post.all
-
 		get :index
 		#puts @response.body
 		assert_response :success
 		assert_select '.contentBox', minimum: 5
 	end
+
+	test "should get at least first page"	do
+		get(:page, {'page' => 0})
+		assert_response :success
+		assert_select '.contentBox', minimum: 5
+	end
+
+	# TODO: fix so that 15 is dynamic in sitewide variable
+	test "should get second page if greater than 15 posts" do
+		if Post.all.count > 15
+			get(:page, {'page' => 0})
+    	assert_response :success
+    	assert_select '.contentBox', minimum: 15
+		end
+	end
+
 end
