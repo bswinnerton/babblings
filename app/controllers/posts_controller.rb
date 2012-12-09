182bd03cf536b5509419b97fbce52d96b7659f12
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-09 22:28:31 +0000

    functionality for (un)delete and (un)hide

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,15 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.where(:is_deleted => 0, :is_hidden => 0).limit(Rails.configuration.items_per_page).order('created_at DESC')
+    @posts = Post.where(:is_deleted => false, :is_hidden => false).limit(Rails.configuration.items_per_page).order('created_at DESC')
   end
 
   def show
-    @posts = Post.find(params[:id], :conditions => 'is_deleted = 0')
+    @posts = Post.find(params[:id], :conditions => ['is_deleted = ?', false])
   end
 
   def page
-    @posts = Post.where(:is_deleted => 0, :is_hidden => 0).limit(Rails.configuration.items_per_page).offset(params[:page].to_i * Rails.configuration.items_per_page).order('created_at DESC')
+    @posts = Post.where(:is_deleted => false, :is_hidden => false).limit(Rails.configuration.items_per_page).offset(params[:page].to_i * Rails.configuration.items_per_page).order('created_at DESC')
     render :layout => false
   end
   
@@ -21,8 +21,34 @@ class PostsController < ApplicationController
     @post = Post.new(
       author: '1',
       content: params[:post][:content],
-      post_type: params[:post][:content]
+      post_type: params[:post][:content],
     )
     @post.save
+		render :action => :success
   end
+
+	def delete
+		@post = Post.update(params[:id], :is_deleted => true)
+		@post.save
+		render :action => :success
+	end
+
+	def undelete
+		@post = Post.update(params[:id], :is_deleted => false)
+		@post.save
+		render :action => :success
+	end
+
+	def hide
+		@post = Post.update(params[:id], :is_hidden => true)
+		@post.save
+		render :action => :success
+	end
+
+	def unhide
+		@post = Post.update(params[:id], :is_hidden => false)
+		@post.save
+		render :action => :success
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Babblings::Application.routes.draw do
   root :to => 'posts#index'
   resources :posts, :except => :destroy
-  match "/posts/page/:page" => "posts#page"
-  match "/help" => "help#index"
+  match '/posts/page/:page' => 'posts#page'
+	match '/posts/delete/:id' => 'posts#delete'
+	match '/posts/undelete/:id' => 'posts#undelete'
+	match '/posts/hide/:id' => 'posts#hide'
+	match '/posts/unhide/:id' => 'posts#unhide'
+  match '/help' => 'help#index'
 end
