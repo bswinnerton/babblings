1f46c4503b9bac92b9e975f8febf569df054be9f
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-14 06:52:13 +0000

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121213040313) do
+ActiveRecord::Schema.define(:version => 20121214051709) do
 
   create_table "posts", :force => true do |t|
     t.integer  "author"
