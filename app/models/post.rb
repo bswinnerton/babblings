ac096938047a233c64b8404bd389c8092eab4ce5
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-18 17:17:11 +0000

    refactoring

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,25 +12,25 @@ class Post < ActiveRecord::Base
 
   def set_values
     # Image
-    if self.content =~ /(\.jpg|\.png|\.bmp|\.gif)$/
+    if content =~ /(\.jpg|\.png|\.bmp|\.gif)$/
       self.post_type = 'image'
       self.original_path = self.content
     # YouTube
-    elsif self.content =~ /(#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be\/)[^&\n]+#)/
+    elsif content =~ /(#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be\/)[^&\n]+#)/
       self.post_type = 'youtube'
       self.original_path = self.content
       self.content = $1
       self.width = 280
       self.height = 158
     # Vimeo
-    elsif self.content =~ /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/
+    elsif content =~ /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/
       self.post_type = 'vimeo'
       self.original_path = self.content
       self.content = $5
       self.width = 280
       self.height = 158
     # Spotify
-    elsif self.content =~ /spotify:track:([a-zA-Z0-9]{22})/
+    elsif content =~ /spotify:track:([a-zA-Z0-9]{22})/
       self.post_type = 'spotify'
       self.original_path = self.content
       self.content = $1
