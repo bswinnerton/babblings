231660886723220a66f8ad1a876275b547daa616
Brooks Swinnerton <bswinnerton@gmail.com>
2012-12-20 21:03:19 +0000

    making routes more RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Babblings::Application.routes.draw do
   root :to => 'posts#index'
-  resources :posts, :except => :destroy
+  resources :posts do
+    member do
+      put   'undelete'    => 'posts#undestroy'
+      put   'hide'        => 'posts#hide'
+      put   'unhide'      => 'posts#unhide'
+    end
+  end
   match '/posts/page/:page' => 'posts#page'
-	match '/posts/delete/:id' => 'posts#delete'
-	match '/posts/undelete/:id' => 'posts#undelete'
-	match '/posts/hide/:id' => 'posts#hide'
-	match '/posts/unhide/:id' => 'posts#unhide'
   match '/help' => 'help#index'
 end
