f9f45d64c74bf09ea8c55d6aeb520ec81c7b682b
Brooks Swinnerton <bswinnerton@gmail.com>
2013-09-09 20:46:34 -0400

    Adding Post#show

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,19 +4,47 @@ describe PostsController do
   describe 'GET #index' do
     it "responds successfully with an HTTP 200 status code" do
       get :index
+
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
 
     it "renders the index template" do
       get :index
+
       expect(response).to render_template :index
     end
 
     it "loads all of the posts into @posts" do
       post1, post2 = FactoryGirl.create(:post), FactoryGirl.create(:post)
       get :index
+
       expect(assigns(:posts)).to match_array([post1, post2])
+    end
+  end
+
+  describe 'GET #show' do
+    before :each do
+      @post1 = FactoryGirl.create(:post)
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :show, id: @post1.id
+
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the show template" do
+      get :show, id: @post1.id
+
+      expect(response).to render_template :show
+    end
+
+    it "loads the post into @post" do
+      get :show, id: @post1.id
+
+      expect(assigns(:post)).to eq(@post1)
     end
   end
 end
