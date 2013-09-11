85306f3bd3de82f4bb8d839c3da67a9c3b896329
Brooks Swinnerton <bswinnerton@gmail.com>
2013-09-11 12:56:01 -0400

    Adding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create([
+  {
+    user_id: 1,
+    format: 'image',
+    content: 'http://s3.amazonaws.com/s3.babblin.gs/posts/images/000/000/260/large/tumblr_msnl3ayMxU1rsnzy2o5_1280.jpg',
+    width: 960,
+    height: 962,
+    is_deleted: false
+  },
+  {
+    user_id: 1,
+    format: 'image',
+    content: 'http://s3.amazonaws.com/s3.babblin.gs/posts/images/000/000/257/large/24ekOAH.jpg',
+    width: 960,
+    height: 1280,
+    is_deleted: false
+  },
+  {
+    user_id: 1,
+    format: 'image',
+    content: 'http://s3.amazonaws.com/s3.babblin.gs/posts/images/000/000/248/large/tumblr_mqeom2a2oU1qbltjyo2_1280.jpg',
+    width: 960,
+    height: 960,
+    is_deleted: false
+  },
+  {
+    user_id: 1,
+    format: 'image',
+    content: 'http://s3.amazonaws.com/s3.babblin.gs/posts/images/000/000/244/large/3CjBFlN.jpg',
+    width: 960,
+    height: 607,
+    is_deleted: false
+  },
+  {
+    user_id: 1,
+    format: 'image',
+    content: 'http://s3.amazonaws.com/s3.babblin.gs/posts/images/000/000/241/large/OoRsR42.gif',
+    width: 960,
+    height: 960,
+    is_deleted: false
+  }
+])
