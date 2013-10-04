c4ab6adedc99ea25872b7d9c1cb7065674ed5b28
Brooks Swinnerton <bswinnerton@gmail.com>
2013-10-04 10:37:40 -0400

    Removing image calculation for lack of responsiveness

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,21 +1,2 @@
 module PostsHelper
-  SIZES = {
-    small: 210,
-    medium: 284,
-    large: 350
-  }
-
-  def calculate_width(width = SIZES[:large])
-    width
-  end
-
-  def calculate_height(width, height)
-    (SIZES[:large] * calculate_ratio(width, height)).to_i
-  end
-
-private
-
-  def calculate_ratio(width, height)
-    height.to_f / width.to_f
-  end
 end
