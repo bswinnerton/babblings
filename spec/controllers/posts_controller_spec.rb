60b609f56bd85aa3866427091f3cf5e28a505daf
Brooks Swinnerton <bswinnerton@gmail.com>
2013-10-04 10:32:48 -0400

    Fixing spacing in specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,21 +4,18 @@ describe PostsController do
   describe 'GET #index' do
     it "responds successfully with an HTTP 200 status code" do
       get :index
-
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
 
     it "renders the index template" do
       get :index
-
       expect(response).to render_template :index
     end
 
     it "loads all of the posts into @posts" do
       post1, post2 = FactoryGirl.create(:post), FactoryGirl.create(:post)
       get :index
-
       expect(assigns(:posts)).to match_array([post1, post2])
     end
   end
@@ -30,20 +27,17 @@ describe PostsController do
 
     it "responds successfully with an HTTP 200 status code" do
       get :show, id: @post1.id
-
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
 
     it "renders the show template" do
       get :show, id: @post1.id
-
       expect(response).to render_template :show
     end
 
     it "loads the post into @post" do
       get :show, id: @post1.id
-
       expect(assigns(:post)).to eq(@post1)
     end
   end
@@ -51,21 +45,18 @@ describe PostsController do
   describe 'GET #new' do
     it "responds successfully with an HTTP 200 status code" do
       get :new
-
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
 
     it "renders the new template" do
       get :new
-
       expect(response).to render_template :new
     end
 
     it "creates a new instance of Post" do
       @post = Post.new
       get :new
-
       expect(assigns(:post)).to_not eq(nil)
     end
   end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -8,26 +8,22 @@ describe "posts" do
 
   it "displays all of the posts" do
     visit root_path
-
     expect(page).to have_css 'img', @post1.content
     expect(page).to have_css 'img', @post2.content
   end
 
   it "displays individual posts" do
     visit post_path(@post1)
-
     expect(page).to have_css 'img', @post1.content
   end
 
   it "links to original content on show page" do
     visit post_path(@post1)
-
     expect(page).to have_selector "a[href='#{@post1.original_path}']"
   end
 
   it "shows the new post page" do
     visit new_post_path
-
     expect(page).to have_field('post_content', with: 'enter your text, image, or video')
   end
 end
