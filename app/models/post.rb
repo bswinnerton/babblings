e3131d81078173f4e386efd061fdd80f613a4f2e
Brooks Swinnerton <bswinnerton@gmail.com>
2013-10-15 13:14:51 -0400

    DRYing up model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 class Post < ActiveRecord::Base
   before_save :determine_format, :pull_image
   scope :recent, -> { order(created_at: :desc) }
-  has_attached_file :image, styles: { thumbnail: '350x', large: '1000x' }
   validates_presence_of :content
+  has_attached_file :image, styles: { thumbnail: '350x', large: '1000x' }
 
   SUPPORTED_FORMATS = %w(image youtube vimeo quote spotify soundcloud definition)
 
   def determine_format
-    self.format = self.content.match(/(#{SUPPORTED_FORMATS.join("|")}):(.*)$/).captures.first
-    self.content = self.content.match(/(#{SUPPORTED_FORMATS.join("|")}):(.*)$/).captures.second
+    supported_formats_in_regex = SUPPORTED_FORMATS.join("|")
+    self.format = self.content.match(/(#{supported_formats_in_regex}):(.*)$/).captures.first
+    self.content = self.content.match(/(#{supported_formats_in_regex}):(.*)$/).captures.second
   end
 
   def pull_image
