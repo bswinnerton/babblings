20ce8b0e3974fe22232739fdc8f46998659008bd
Brooks Swinnerton <bswinnerton@gmail.com>
2013-10-25 12:50:40 -0400

    Fixing simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+require 'simplecov'
+SimpleCov.start
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'paperclip/matchers'
-require 'simplecov'
 #require 'capybara/poltergeist'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -13,8 +14,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
-
-SimpleCov.start
 
 RSpec.configure do |config|
   # ## Mock Framework
