df1ddc5e859ec1982af3f1703deb4264671ea518
Brooks Swinnerton <bswinnerton@gmail.com>
2014-11-22 00:38:08 -0500

    Restructuring API response of typed resources

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,17 +1,25 @@
 class Api::V1::PostsController < ApplicationController
   def index
     @posts = post_query.active.ordered.page(params[:page])
-    render json: {posts: @posts}
+    render json: {resource => @posts}
   end
 
   def show
     @post = post_query.find(params[:id])
-    render json: {posts: @post}
+    render json: {resource => @post}
   end
 
 private
 
   def post_query
-    params[:type] ? Post.where(type: params[:type]) : Post
+    type == 'Post' ? Post : Post.where(type: type)
+  end
+
+  def resource
+    type.downcase.pluralize
+  end
+
+  def type
+    params[:type] || 'Post'
   end
 end
