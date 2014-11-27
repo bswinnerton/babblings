5c457b549708f8a3f8a6648d955f2862a4496d3c
Brooks Swinnerton <bswinnerton@gmail.com>
2014-11-26 23:08:14 -0500

    Updating layout of serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,15 +1,17 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :type, :content, :width, :height, :ratio, :created_at, :updated_at, :image_updated_at, :deleted_at, :image_processing, :thumbnail_image, :full_image
+  attributes :id, :type, :content, :width, :height, :ratio, :created_at,
+    :updated_at, :image_updated_at, :deleted_at, :image_processing,
+    :thumbnail_image, :full_image
 
   private
 
   def thumbnail_image
-    return unless object.type == 'Picture'
+    return unless object.is_a? Picture
     object.image(:thumbnail)
   end
 
   def full_image
-    return unless object.type == 'Picture'
+    return unless object.is_a? Picture
     object.image(:full)
   end
 end
